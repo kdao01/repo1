f0d8db97a49d4a31fd5369affc35f10ac153fbb4
kenny dao <kdao01@gmail.com>
2016-08-25 16:30:15 -0400

    v2

---
+++ HellorRevised.java
@@ -0,0 +1,8 @@
+public class HelloRevised{
+ 
+   public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello Clone!" );
+      }
+	  }
